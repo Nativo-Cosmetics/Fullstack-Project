d505e43adce304b799d71cf0859b3a21e7aff5db
Mattiud <mstias.sanhueza30@gmail.com>
2026-05-27 19:49:59 -0400

    Front del catalogo listo con catalog.js simulando base de datos

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/index.jsx'
+import Catalog from './components/store/catalog.jsx'
 
 import './global.css'
 
@@ -10,6 +11,7 @@ function App() {
       
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/catalog' element={<Catalog />} />
       </Routes>
     </>
   )
